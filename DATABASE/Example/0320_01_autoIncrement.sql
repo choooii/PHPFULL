f45cb045356d41f04d991609cc0c4881f7f4a082
choooii <dkfks0832@gmail.com>
2023-03-21 09:23:15 +0900

    0320 upd

---
+++ DATABASE/Example/0320_01_autoIncrement.sql
@@ -0,0 +1,55 @@
+-- 1. auto_increment 란?
+-- 	자동 증가 값을 가지는 컬럼으로  값을 직접 대입할 수 없습니다.
+-- 	중간에 값을 삭제한다고 해서, 삭제된 값을 재사용 하지 않으며
+-- 	레코드가 적재될 때마다 1씩 증가하게 됩니다.
+--  	PK에만 적용할 수 있습니다.
+
+-- 2. auto_increment 생성
+CREATE TABLE test (
+	mem_no		INT(11) PRIMARY KEY AUTO_INCREMENT
+	,mem_name	VARCHAR(50)
+);
+
+INSERT INTO test (mem_name)
+VALUES ('최아란');
+INSERT INTO test (mem_name)
+VALUES ('박상준');
+
+SELECT *
+FROM test;
+
+DELETE FROM test
+WHERE mem_name = '박상준';
+
+-- 3. auto_increment 수정
+--	- 이미 생성한 테이블의 컬럼에 추가할 때
+CREATE TABLE test (
+	mem_no		INT(11) PRIMARY KEY AUTO_INCREMENT
+	,mem_name	VARCHAR(50)
+);
+
+ALTER TABLE test MODIFY mem_no INT(11) NOT NULL AUTO_INCREMENT;
+
+INSERT INTO test (mem_name)
+VALUES ('최아란');
+INSERT INTO test (mem_name)
+VALUES ('박상준');
+
+SELECT *
+FROM test;
+
+--	- auto_increment의 값을 초기화 할 때
+ALTER TABLE test AUTO_INCREMENT = 10;
+
+INSERT INTO test (mem_name)
+VALUES ('최아란');
+
+SELECT *
+FROM test;
+
+-- 다른 초기화 방법
+TRUNCATE TABLE 테이블명;
+
+-- 4. auto_increment사용시 주의사항
+-- 	스토리지 엔진인 InnoDB 의 경우 SELECT MAX(num) 구문을 사용하여 auto_increment 값을 새로 저장하는데,
+-- 	이 때문에 증가 값이 다른 테이블들과 종속되는 값을 가지면서 서로 달라지게 되면 문제가 생길수도 있으니 주의하여 사용해야 합니다.
