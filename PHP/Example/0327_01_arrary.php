22cc1195a2dc2ca86e28d9ae660fca92dcf41428
choooii <dkfks0832@gmail.com>
2023-03-27 18:07:49 +0900

    0327

--- PHP/Example/0327_01_arrary.php
+++ PHP/Example/0327_01_arrary.php
@@ -45,7 +45,65 @@
     // 배열의 원소 삭제 : unset()
     $arr_week = array("Sun", "Mon", "delete", "Tue", "Wed");
     unset($arr_week[2]);
-    print_r($arr_week);
+    // print_r($arr_week);
 
+    // 중복되지 않는 원소를 반환 : arrary_diff(기준, 비교)
+    $arr_diff_1 = array("a", "b", "c");
+    $arr_diff_2 = array("a", "b", "d");
+
+    $arr_diff = array_diff($arr_diff_1, $arr_diff_2);
+    // print_r($arr_diff);
+
+    // 배열의 정렬 : asort(), arsort(), ksort(), krsort()
+    // asort();
+    $arr_asort = array("b", "a", "d", "c");
+    asort($arr_asort);
+    // print_r($arr_asort);
+
+    // arsort();
+    $arr_arsort = array("b", "a", "d", "c");
+    arsort($arr_arsort);
+    // print_r($arr_arsort);
+
+    // ksort();
+    $arr_ksort = array("key1" => "val1"
+                        ,"key3" => "val3"
+                        ,"key4" => "val4"
+                        ,"key2" => "val2"
+                    );
+    ksort($arr_ksort);
+    // print_r($arr_ksort);
+
+    // krsort();
+    $arr_krsort = array("key1" => "val1"
+                        ,"key3" => "val3"
+                        ,"key4" => "val4"
+                        ,"key2" => "val2"
+    );
+    krsort($arr_krsort);
+    // print_r($arr_krsort);
+
+    // array의 사이즈를 반환하는 함수 count();
+    // echo count($arr_krsort);
+
+    //foreach문 
+    // foreach( $array as $key => $val){}
+    // foreach( $array as $val){}
+    $arr1 = array("key1" => "val1"
+                ,"key3" => "val3"
+                ,"key4" => "val4"
+                ,"key2" => "val2"
+                );
     
+    foreach($arr1 as $key => $val)
+    {
+        echo $key." : ".$val."\n";
+    }
+
+    foreach($arr1 as $val)
+    {
+        echo $val."\n";
+    }
+
+
     ?>
