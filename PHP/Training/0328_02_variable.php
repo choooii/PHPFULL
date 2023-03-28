19b8f555a8e869bfd553f9fd25cd9ce0259db6a5
choooii <dkfks0832@gmail.com>
2023-03-28 18:10:17 +0900

    0328

--- PHP/Training/0328_02_variable.php
+++ PHP/Training/0328_02_variable.php
@@ -23,6 +23,7 @@
     // echo my_len2($arr_base);
 
 
+    // 별을 int 수만큼 출력하는 함수
     function print_star($int)
     {
         for ($i=0; $i < $int ; $i++) 
@@ -32,14 +33,20 @@
         echo "\n";
     }
 
-    function print_echo($num)
+    // *
+    // **
+    // ***
+    function print_echo($int)
     {
-        for ($i=1; $i <= $num ; $i++) 
+        for ($i=1; $i <= $int ; $i++) 
         { 
             print_star($i);
         }
     }
 
+    // ***
+    // ***
+    // ***
     function print_star_rect($int)
     {
         for ($i=1; $i <= $int ; $i++) 
@@ -48,8 +55,8 @@
         }
     }
 
-    // echo print_echo(5);
-    echo print_star_rect(5);
+    echo print_echo(3);
+    // echo print_star_rect(3);
 
 
 
